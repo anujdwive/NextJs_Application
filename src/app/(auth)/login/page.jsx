cd3fb3108c07213a7e3ab532a1d3b1a1b121ef02
Anuj Dwivedi <anujdwivedi0705@gmail.com>
2026-08-29 21:06:50 +0530

    learn useRouter

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,7 +1,29 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import React from "react";
 
+function loginUser() {
+  return true;
+}
+
 const Login = () => {
-  return <div>Login</div>;
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    const response = await loginUser();
+    if (response) {
+      console.log("Transfer");
+      router.push("/");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
 };
 
 export default Login;
