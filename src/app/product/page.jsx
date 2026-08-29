ad0f3dfbc5d0d77f791bd3a5d4e3122da11cfbb4
Anuj Dwivedi <anujdwivedi0705@gmail.com>
2026-08-29 15:00:11 +0530

    learn about suspense and streaming

--- src/app/product/page.jsx
+++ src/app/product/page.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { Suspense } from "react";
+
+export const ProductDetails = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  return <h1>Product Loaded</h1>;
+};
+
+export const CartDetails = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  return <h1>Cart Loaded</h1>;
+};
 
 const Product = () => {
-  return <div>Product</div>;
+  return (
+    <div>
+      <Suspense fallback={<div>Product details loading....</div>}>
+        <ProductDetails />
+      </Suspense>
+      <Suspense fallback={<div>Cart details loading....</div>}>
+        <CartDetails />
+      </Suspense>
+    </div>
+  );
 };
 
 export default Product;
